0552c5d4d472007b776451671b70fc7ee4cace9c
Liam Paul <liam@pop-stack.org>
2021-05-29 20:23:55 -0700

    put clock on thread

--- src/main.c
+++ src/main.c
@@ -1,10 +1,26 @@
 #include <ncurses.h>
 #include <pthread.h>
 #include <time.h>
+#include <unistd.h>
 
-void * sup(){
-  mvprintw(2, 0, "sup");
+volatile bool done = FALSE;
+
+void * input( char * ch){
+  while(*ch != 27){
+    *ch = getch();
+  }
+  done = TRUE;
   return NULL;
+}
+
+void * timer(time_t * t){
+  time_t stime, ctime;
+  stime = time(NULL);
+  while(TRUE){
+    ctime = time(NULL);
+    *t = ctime - stime;
+    sleep(1);
+  }
 }
 
 int main()
@@ -12,19 +28,30 @@ int main()
   initscr();
   noecho();
   raw();
+  curs_set(0);
 
-  pthread_t thread;
-  pthread_create(&thread, NULL, sup, NULL);
+  int ch = ' ';
+  pthread_t iThread;
+  pthread_create(&iThread, NULL, input, &ch);
 
-  time_t stime, ctime, dtime;
-  stime = time(NULL);
-  while(TRUE){
-    ctime = time(NULL);
-    dtime = ctime - stime;
-    mvprintw(0,0,"%ld", dtime);
+  pthread_t tThread;
+  time_t dtime;
+  pthread_create(&tThread, NULL, timer, &dtime);
+
+  char * clr[COLS];
+  WINDOW * clock;
+  clock = newwin(1, 8, 0, COLS - 8);
+
+  while(!done){
+
+    wmove(clock, 0, 0);
+    wprintw(clock, "        ");
+    wmove(clock, 0, 0);
+    wprintw(clock, "%ld:%ld:%ld", dtime/(60*60),(dtime/60)%60, dtime%60);
+    wrefresh(clock);
+
+    mvprintw(0,0,"%c",ch);
     refresh();
-    if(dtime == 10)
-      break;
   }
 
   endwin();
